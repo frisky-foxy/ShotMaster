ae5abba9a2a6f402d1a63afb9a44bcac7f8a9d2a
resteem <eremeevst@gmail.com>
2013-10-05 21:29:19 +0400

    toggleByTag

--- prjShotMaster/Forms/frmMain.Designer.cs
+++ prjShotMaster/Forms/frmMain.Designer.cs
@@ -152,28 +152,27 @@
             // 
             // cmsNotify
             // 
-            resources.ApplyResources(this.cmsNotify, "cmsNotify");
             this.cmsNotify.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.closeToolStripMenuItem});
             this.cmsNotify.Name = "contextMenuStrip1";
+            resources.ApplyResources(this.cmsNotify, "cmsNotify");
             // 
             // closeToolStripMenuItem
             // 
-            resources.ApplyResources(this.closeToolStripMenuItem, "closeToolStripMenuItem");
             this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            resources.ApplyResources(this.closeToolStripMenuItem, "closeToolStripMenuItem");
             this.closeToolStripMenuItem.Click += new System.EventHandler(this.exitApp);
             // 
             // msMainMenu
             // 
-            resources.ApplyResources(this.msMainMenu, "msMainMenu");
             this.msMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsmiFile,
             this.helpToolStripMenuItem});
+            resources.ApplyResources(this.msMainMenu, "msMainMenu");
             this.msMainMenu.Name = "msMainMenu";
             // 
             // tsmiFile
             // 
-            resources.ApplyResources(this.tsmiFile, "tsmiFile");
             this.tsmiFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.stopToolStripMenuItem,
             this.startToolStripMenuItem,
@@ -182,54 +181,57 @@
             this.toolStripMenuItem1,
             this.exitToolStripMenuItem});
             this.tsmiFile.Name = "tsmiFile";
+            resources.ApplyResources(this.tsmiFile, "tsmiFile");
             // 
             // stopToolStripMenuItem
             // 
-            resources.ApplyResources(this.stopToolStripMenuItem, "stopToolStripMenuItem");
             this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            resources.ApplyResources(this.stopToolStripMenuItem, "stopToolStripMenuItem");
+            this.stopToolStripMenuItem.Tag = "Stop";
             this.stopToolStripMenuItem.Click += new System.EventHandler(this.Stop);
             // 
             // startToolStripMenuItem
             // 
-            resources.ApplyResources(this.startToolStripMenuItem, "startToolStripMenuItem");
             this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            resources.ApplyResources(this.startToolStripMenuItem, "startToolStripMenuItem");
+            this.startToolStripMenuItem.Tag = "Start";
             this.startToolStripMenuItem.Click += new System.EventHandler(this.Start);
             // 
             // shotNowToolStripMenuItem
             // 
-            resources.ApplyResources(this.shotNowToolStripMenuItem, "shotNowToolStripMenuItem");
             this.shotNowToolStripMenuItem.Name = "shotNowToolStripMenuItem";
+            resources.ApplyResources(this.shotNowToolStripMenuItem, "shotNowToolStripMenuItem");
             this.shotNowToolStripMenuItem.Click += new System.EventHandler(this.Shot);
             // 
             // openDestinationFolderToolStripMenuItem
             // 
-            resources.ApplyResources(this.openDestinationFolderToolStripMenuItem, "openDestinationFolderToolStripMenuItem");
             this.openDestinationFolderToolStripMenuItem.Name = "openDestinationFolderToolStripMenuItem";
+            resources.ApplyResources(this.openDestinationFolderToolStripMenuItem, "openDestinationFolderToolStripMenuItem");
             this.openDestinationFolderToolStripMenuItem.Click += new System.EventHandler(this.openDestFolder);
             // 
             // toolStripMenuItem1
             // 
-            resources.ApplyResources(this.toolStripMenuItem1, "toolStripMenuItem1");
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            resources.ApplyResources(this.toolStripMenuItem1, "toolStripMenuItem1");
             // 
             // exitToolStripMenuItem
             // 
-            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitApp);
             // 
             // helpToolStripMenuItem
             // 
-            resources.ApplyResources(this.helpToolStripMenuItem, "helpToolStripMenuItem");
             this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.gitHubToolStripMenuItem,
             this.aboutToolStripMenuItem});
             this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            resources.ApplyResources(this.helpToolStripMenuItem, "helpToolStripMenuItem");
             // 
             // gitHubToolStripMenuItem
             // 
-            resources.ApplyResources(this.gitHubToolStripMenuItem, "gitHubToolStripMenuItem");
             this.gitHubToolStripMenuItem.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.gitHubToolStripMenuItem, "gitHubToolStripMenuItem");
             this.gitHubToolStripMenuItem.ForeColor = System.Drawing.Color.Blue;
             this.gitHubToolStripMenuItem.Name = "gitHubToolStripMenuItem";
             this.gitHubToolStripMenuItem.URI = null;
@@ -242,13 +244,9 @@
             // 
             // ntfIcn
             // 
-            resources.ApplyResources(this.ntfIcn, "ntfIcn");
             this.ntfIcn.ContextMenuStrip = this.cmsNotify;
+            resources.ApplyResources(this.ntfIcn, "ntfIcn");
             this.ntfIcn.DoubleClick += new System.EventHandler(this.ntfIcn_DoubleClick);
-            // 
-            // fbd
-            // 
-            resources.ApplyResources(this.fbd, "fbd");
             // 
             // tmrDefault
             // 
@@ -261,39 +259,41 @@
             // 
             // ss
             // 
-            resources.ApplyResources(this.ss, "ss");
             this.ss.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsslblCountdown});
+            resources.ApplyResources(this.ss, "ss");
             this.ss.Name = "ss";
             this.ss.SizingGrip = false;
             // 
             // tsslblCountdown
             // 
-            resources.ApplyResources(this.tsslblCountdown, "tsslblCountdown");
             this.tsslblCountdown.Name = "tsslblCountdown";
+            resources.ApplyResources(this.tsslblCountdown, "tsslblCountdown");
             // 
             // toolStrip1
             // 
-            resources.ApplyResources(this.toolStrip1, "toolStrip1");
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsbtnStart,
             this.tsbtnStop});
+            resources.ApplyResources(this.toolStrip1, "toolStrip1");
             this.toolStrip1.Name = "toolStrip1";
             // 
             // tsbtnStart
             // 
-            resources.ApplyResources(this.tsbtnStart, "tsbtnStart");
             this.tsbtnStart.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.tsbtnStart.ForeColor = System.Drawing.Color.Green;
             this.tsbtnStart.Name = "tsbtnStart";
+            resources.ApplyResources(this.tsbtnStart, "tsbtnStart");
+            this.tsbtnStart.Tag = "Start";
             this.tsbtnStart.Click += new System.EventHandler(this.Start);
             // 
             // tsbtnStop
             // 
-            resources.ApplyResources(this.tsbtnStop, "tsbtnStop");
             this.tsbtnStop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.tsbtnStop.ForeColor = System.Drawing.Color.Red;
             this.tsbtnStop.Name = "tsbtnStop";
+            resources.ApplyResources(this.tsbtnStop, "tsbtnStop");
+            this.tsbtnStop.Tag = "Stop";
             this.tsbtnStop.Click += new System.EventHandler(this.Stop);
             // 
             // frmMain
